bcce9a085107d5d84268d299f02495b80adbb203
mnicky <xmnicky@gmail.com>
2012-10-19 18:29:59 +0200

    proper read/write + stream tests added

--- src/bfcks/simple.clj
+++ src/bfcks/simple.clj
@@ -6,23 +6,22 @@
     (interpret code 30000))
   ([code cell-size]
     (interpret code cell-size *in* *out*))
-  ([code cell-size input-stream output-stream]
+  ([code cell-size in out]
     (let [code (vec code)]
-      (binding [*in* input-stream *out* output-stream]
-        (loop [instr-p 0 cell-p 0 cells (vec (repeat cell-size 0))]
-          (if (>= instr-p (count code))
-            cells
-            (case (code instr-p)
-              \> (recur (inc instr-p) (inc cell-p) cells)
-              \< (recur (inc instr-p) (dec cell-p) cells)
-              \+ (recur (inc instr-p) cell-p (assoc cells cell-p (inc (cells cell-p))))
-              \- (recur (inc instr-p) cell-p (assoc cells cell-p (dec (cells cell-p))))
-              \. (do (print (char (cells cell-p)))
-                     (recur (inc instr-p) cell-p cells))
-              \, (recur (inc instr-p) cell-p (assoc cells cell-p (.read *in*)))
-              \[ (recur (inc instr-p) cell-p cells)
-              \] (recur (inc instr-p) cell-p cells)
-              (recur (inc instr-p) cell-p cells))))))))
+      (loop [instr-p 0 cell-p 0 cells (vec (repeat cell-size 0))]
+        (if (>= instr-p (count code))
+          cells
+          (case (code instr-p)
+            \> (recur (inc instr-p) (inc cell-p) cells)
+            \< (recur (inc instr-p) (dec cell-p) cells)
+            \+ (recur (inc instr-p) cell-p (assoc cells cell-p (inc (cells cell-p))))
+            \- (recur (inc instr-p) cell-p (assoc cells cell-p (dec (cells cell-p))))
+            \. (do (.write out (cells cell-p))
+                   (recur (inc instr-p) cell-p cells))
+            \, (recur (inc instr-p) cell-p (assoc cells cell-p (.read in)))
+            \[ (recur (inc instr-p) cell-p cells)
+            \] (recur (inc instr-p) cell-p cells)
+            (recur (inc instr-p) cell-p cells)))))))
 
 (defn foo
   "I don't do a whole lot."
--- test/bfcks/simple_test.clj
+++ test/bfcks/simple_test.clj
@@ -3,10 +3,17 @@
         bfcks.simple))
 
 (deftest basic-interpret-test
-    (is (= [0 0 0] (interpret "" 3)))
-    (is (= [1 0 0] (interpret "+" 3)))
-    (is (= [0 1 0] (interpret ">+" 3)))
-    (is (= [0 0 -1] (interpret ">>-" 3)))
-    (is (= [3 0 0] (interpret "><+++" 3)))
-    (is (= [2 0 0] (interpret "++nocode" 3)))
-  )
+  (is (= [0 0 0] (interpret "" 3)))
+  (is (= [1 0 0] (interpret "+" 3)))
+  (is (= [0 1 0] (interpret ">+" 3)))
+  (is (= [0 0 -1] (interpret ">>-" 3)))
+  (is (= [3 0 0] (interpret "><+++" 3)))
+  (is (= [2 0 0] (interpret "++nocode" 3)))
+)
+
+(deftest stream-interpret-test
+  (let [in  (java.io.ByteArrayInputStream. (byte-array (map byte [97 98 99])))
+        out (java.io.ByteArrayOutputStream.)]
+    (is (and (= [97 98 99] (interpret ",.>,.>,." 3 in out))
+             (= "abc" (str out))))
+))
