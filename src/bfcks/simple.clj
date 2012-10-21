d149264b2e00c32508d5323c93829c59974adb0b
mnicky <xmnicky@gmail.com>
2012-10-22 00:52:08 +0200

    Commands '[ ]' implemented.

--- src/bfcks/simple.clj
+++ src/bfcks/simple.clj
@@ -1,5 +1,19 @@
 (ns bfcks.simple)
 
+(defn skip
+  "Skips all following instructions until the end of the [...] block and
+  returns the instruction pointer after that block.
+  instr-p is a pointer to the instruction where the [...] block (that shall
+  be skipped) begins."
+  [instr-p code]
+  (loop [instr-p instr-p stack [] first-run true]
+    (if (and (empty? stack) (not first-run))
+      instr-p
+      (case (code instr-p)
+        \[ (recur (inc instr-p) (conj stack :skipped) false)
+        \] (recur (inc instr-p) (pop stack) false)
+        (recur (inc instr-p) stack false)))))
+
 (defn interpret
   "Interprets given bfck code and returns the resulting cells."
   ([code]
@@ -8,23 +22,26 @@
     (interpret code cell-size *in* *out*))
   ([code cell-size in out]
     (let [code (vec code)]
-      (loop [instr-p 0 cell-p 0 cells (vec (repeat cell-size 0))]
+      (loop [instr-p 0 cell-p 0 cells (vec (repeat cell-size 0)) stack []]
         (if (>= instr-p (count code))
           cells
-          (let [wrap #(mod % 256)]
+          (let [wrap #(mod % 256)
+                cell (cells cell-p)]
             (case (code instr-p)
-              \> (recur (inc instr-p) (inc cell-p) cells)
-              \< (recur (inc instr-p) (dec cell-p) cells)
-              \+ (recur (inc instr-p) cell-p (assoc cells cell-p (inc (cells cell-p))))
-              \- (recur (inc instr-p) cell-p (assoc cells cell-p (dec (cells cell-p))))
+              \> (recur (inc instr-p) (inc cell-p) cells stack)
+              \< (recur (inc instr-p) (dec cell-p) cells stack)
+              \+ (recur (inc instr-p) cell-p (assoc cells cell-p (wrap (inc cell))) stack)
+              \- (recur (inc instr-p) cell-p (assoc cells cell-p (wrap (dec cell))) stack)
               \. (do (.write out (cells cell-p))
-                     (recur (inc instr-p) cell-p cells))
-              \, (recur (inc instr-p) cell-p (assoc cells cell-p (.read in)))
-              \[ (recur (inc instr-p) cell-p cells)
-              \] (recur (inc instr-p) cell-p cells)
-              (recur (inc instr-p) cell-p cells)))))))
+                     (recur (inc instr-p) cell-p cells stack))
+              \, (recur (inc instr-p) cell-p (assoc cells cell-p (.read in)) stack)
+              \[ (if (zero? cell)
+                   (recur (skip instr-p code) cell-p cells stack)
+                   (recur (inc instr-p) cell-p cells (conj stack instr-p)))
+              \] (recur (if (zero? cell) (inc instr-p) (peek stack)) cell-p cells (pop stack))
+              (recur (inc instr-p) cell-p cells stack))))))))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(comment
+;print 'Hello World!'
+(dorun (interpret "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."))
+)
--- test/bfcks/simple_test.clj
+++ test/bfcks/simple_test.clj
@@ -6,9 +6,10 @@
   (is (= [0 0 0] (interpret "" 3)))
   (is (= [1 0 0] (interpret "+" 3)))
   (is (= [0 1 0] (interpret ">+" 3)))
-  (is (= [0 0 -1] (interpret ">>-" 3)))
+  (is (= [0 0 255] (interpret ">>-" 3)))
   (is (= [3 0 0] (interpret "><+++" 3)))
-  (is (= [2 0 0] (interpret "++nocode" 3)))
+  (is (= [0 3 0] (interpret ">><+++" 3)))
+  (is (= [2 0 0] (interpret "++no code" 3)))
 )
 
 (deftest stream-interpret-test
@@ -16,4 +17,31 @@
         out (java.io.ByteArrayOutputStream.)]
     (is (and (= [97 98 99] (interpret ",.>,.>,." 3 in out))
              (= "abc" (str out))))
-))
+  ))
+
+(deftest skip-test
+  (is (= 1 (skip 0 (vec "+--+"))))
+  (is (= 4 (skip 0 (vec "[--]+"))))
+  (is (= 7 (skip 0 (vec "[-[-]-]+"))))
+  (is (= 9 (skip 0 (vec "[-[-[]]-]+"))))
+  (is (= 14 (skip 0 (vec "[[---]-[[-]]-]+"))))
+  (is (= 12 (skip 7 (vec "[[---]-[[-]]-]+"))))
+)
+
+(deftest conditionals-test
+  (is (= [0 0 0] (interpret "[]" 3)))
+  (is (= [0 0 0] (interpret "+[-]" 3)))
+  (is (= [0 5 0] (interpret "+++++[->+<]" 3)))
+  (is (= [1 0 0] (interpret "[++++]+" 3)))
+  (is (= [0 0 3] (interpret "+++[->[]>+<<]" 3)))
+  (is (= [0 0 9] (interpret "+++[->++[->+<]>+<<]" 3)))
+)
+
+(deftest hello-world-test
+  (let [out (java.io.ByteArrayOutputStream.)]
+    (is (and (= [0 87 100 33 10] (interpret "++++++++++[>+++++++>++++++++++>+
+                                             ++>+<<<<-]>++.>+.+++++++..+++.>+
+                                             +.<<+++++++++++++++.>.+++.------
+                                             .--------.>+.>." 5 *in* out))
+             (= "Hello World!\n" (str out))))
+  ))
